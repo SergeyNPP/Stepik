7536078cc7bf8fc0c1972d032eaaacb7660fa081
«Sergey» <budanoff.2011@yandex.ru>
2025-05-30 08:49:01 +0400

    Простое число, интересное и строка

--- Prostoe.py
+++ Prostoe.py
@@ -9,3 +9,15 @@ for i in range(a, b + 1):
             count += 1
     if count < 2:
         print(i)
+
+
+a, b = int(input()), int(input())
+
+if a == 1:
+    a += 1
+for num in range(a, b + 1):
+    for d in range(2, num):
+        if num % d == 0:
+            break
+    else:
+        print(num)
